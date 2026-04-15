cfaa94070fcbb0a5f06ba2b153134f564c023690
HellfireDevs <devshellfire@gmail.com>
2026-04-15 09:37:48 +0530

    Create __init__.py

---
+++ CLOUDSERVER/__init__.py
@@ -0,0 +1,10 @@
+# ==========================================
+# CLOUDSERVER PACKAGE INITIALIZATION
+# ==========================================
+
+__version__ = "1.0.0"
+__author__ = "Tera Naam / Brand"
+__description__ = "Core Engine for My Custom Cloud API"
+
+# Is file ko zyada chhedne ki zaroorat nahi hoti.
+# Ye bas Python ko batati hai ki ye folder ek module/package hai.
